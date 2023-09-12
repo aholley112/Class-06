5be3272a0e8f00e046dec2174e7144096cbd2c71
Amanda Feely <130320844+aholley112@users.noreply.github.com>
2023-09-12 13:49:03 -0500

    Organized Exercises
    Organized the exercises individually.

---
+++ Class 6-Exercise 6/main.js
@@ -0,0 +1,9 @@
+// Objective: Understand and implement private fields in classes.
+
+// Instructions:
+
+// Visit this repository.
+// Click Use this template follow by Create a new repository.
+// Give this repository a name of private fields in classes.
+// Make sure this is a public repository.
+// Read the readMe.md file for further instructions.
